cbd6cb4a5fe0fd98e921c2f5a4088116d2e16162
KongThePirate <afonso@ArchLinux.localdomain>
2022-01-22 21:32:40 +0000

    Massive rewrite

--- todo.cpp
+++ todo.cpp
@@ -2,13 +2,15 @@
 #include <fstream>
 #include <cstring>
 #include <filesystem>
+#include "todo.hpp"
 
 using namespace std;
+
+const string user = getenv("USER");
 
 
 void list()
 {
-  string user = getenv("USER");
   ifstream file("/home/" + user + "/.config/todo/list.txt");
       
   string line;
@@ -20,75 +22,52 @@ void list()
   }
 }
 
-int main(int argc, char* argv[]){
-  string user = getenv("USER");
-  if (! filesystem::is_directory("/home/" + user + "/.config/todo")){
+void add(int argc, char* input)
+{
+  ofstream list;
+  list.open ("/home/" + user + "/.config/todo/list.txt", ios_base::app);
+  list << input << endl;
+  list.close();
+}
+
+void clear()
+{
+  string newstr = "/home/" + user + "/.config/todo/list.txt";
+  const char *c = newstr.c_str();
+  remove(c);
+}
+
+void remove(int argc, char* input)
+{
+  int linetoremove = atoi(input);
+  string contents, line;
+  auto infile = ifstream("/home/" + user + "/.config/todo/list.txt");
+  short currentline = 1;
+  for (; getline(infile, line); ++currentline)
+  {
+    if (currentline != linetoremove)
+    {
+    contents += line;
+    }
+  }
+  auto outfile = ofstream("/home/" + user + "/.config/todo/list.txt");
+  outfile << contents;
+}
+
+int main(int argc, char* argv[])
+{
+  // check if ~/.config/todo/ exists, and if not, create it
+  if (! filesystem::is_directory("/home/" + user + "/.config/todo"))
+  {
     filesystem::create_directory("/home/" + user + "/.config/todo");
   }
 
-  // some variables
-  char add1[ ] = "add";
-  char remove1[ ] = "remove";
-  char clear1[ ] = "clear";
-  char list1[ ] = "list";
+  string input(argv[1]);
+
+  if (input == "add" && argc >= 3) add(argc, argv[2]);
+  else if (input == "clear") clear();
+  else if (input == "list") list();
+  else if (input == "remove" && argc >= 3) remove(argc, argv[2]);
   
-// Arguments
-  if (argc >= 2)
-  {
-    if (strcmp(argv[1], add1) == 0)
-    {
-      if (argc >= 3)
-      {
-        string a;
-        a = argv[2];
-        ofstream list;
-        list.open ("/home/" + user + "/.config/todo/list.txt", ios_base::app);
-        list << a << endl;
-        list.close();
-      }
-      else
-      {
-        cout << "todo add: not enough arguments" << endl;
-      }
-    }
-    else if (strcmp(argv[1], clear1) == 0)
-    {
-      string newstr = "/home/" + user + "/.config/todo/list.txt";
-      const char *c = newstr.c_str();
-      remove(c);
-    }
-    else if (strcmp(argv[1], remove1) == 0)
-    {
-      if(argc >= 3)
-      {
-        int linetoremove = atoi(argv[2]);
-        string contents, line;
-        auto infile = ifstream("/home/" + user + "/.config/todo/list.txt");
-        short currentline = 1;
-        for (; getline(infile, line); ++currentline)
-        {
-          if (currentline != linetoremove)
-          {
-            contents += line;
-          }
-        }
-        auto outfile = ofstream("/home/" + user + "/.config/todo/list.txt");
-        outfile << contents;
-      }
-      else{cout << "todo remove: not enough arguments" << endl;}
-    }
-    else if (strcmp(argv[1], list1) == 0)
-    {
-      list();
-    }
-    else
-    {
-      cout << "todo: invalid arguments" << endl;
-    }
-  }
-  else
-  {
-    cout << "todo: missing argument \nCheck 'todo help' for more information." << endl;
-  }
   return 0;
 }
---
+++ todo.hpp
@@ -0,0 +1,6 @@
+void add(int argc, char* input);
+void remove(int argc, char* input);
+void list();
+void clear();
+int main(int argc, char* argv[]);
+
